959b2ce4c136e30194add5c031e1ec0eb4734195
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2026-01-17 09:37:57 -0600

    Fix cpu count in dockerstats

--- girder_sivacor/worker_plugin/lib.py
+++ girder_sivacor/worker_plugin/lib.py
@@ -206,7 +206,7 @@ class DockerStatsCollectorThread(Thread):
 
     @staticmethod
     def calculate_cpu_percent(d):
-        cpu_count = len(d["cpu_stats"]["cpu_usage"].get("percpu_usage", [1]))
+        cpu_count = d["cpu_stats"]["online_cpus"] if "online_cpus" in d["cpu_stats"] else 1
         cpu_percent = 0.0
         cpu_delta = float(d["cpu_stats"]["cpu_usage"]["total_usage"]) - float(
             d["precpu_stats"]["cpu_usage"]["total_usage"]
